88bb54238f2dc0d0befa19d0216180b7f4f3f7f6
Benjamin Boruff <ben@boruff.me>
2017-08-13 16:42:56 -0400

    adding actions

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+  return (
+    [
+      { title: "JS: The Good Parts"},
+      { title: "Harry Potter"},
+      { title: "The Dark Tower"},
+      { title: "Eloquent Ruby"}
+    ]
+  );
+}
